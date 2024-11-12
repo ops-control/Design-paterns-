a9c1839d03833c2bfe3a6e2150962db2d5094448
PC_BU_Jacob <extbua-fh@formation.univ-smb.fr>
2024-11-12 16:16:31 +0100

    ajout de la classe Chapitre

--- objet/Chapitre.java
+++ objet/Chapitre.java
@@ -1,1 +1,30 @@
-public class Chapitre{}
+import java.io.PrintStream;
+
+public class Chapitre{
+    private BlocDeTexte bloc;
+    private TexteCentre titre;
+    public Chapitre(String t){
+        this.bloc=new BlocDeTexte();
+        this.titre=new TexteCentre(t);
+    }
+
+    public void ajoute(Paragraphe p){
+        bloc.ajoute(p);
+    }
+    
+    public void fixeLargeur(int l){
+        bloc.fixeLargeur(l);
+        titre.fixeLargeur(l);
+
+    }
+    public void ecris(PrintStream p){
+        p.println(titre.toString());
+        bloc.ecris(p);
+
+    }
+    public String toString(){
+
+        return "Le Titre di chapitre : "+titre.texte()+"\n"+bloc.toString() ;
+    }
+
+}
